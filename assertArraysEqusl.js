490c0328a543b0d88ff979b9e18904191ad8e7bf
kmiecik013 <kmiecik013@gmail.com>
2021-11-24 05:51:03 +0000

    done part 2

---
+++ assertArraysEqusl.js
@@ -0,0 +1,21 @@
+const eqArrays = function(array1, array2) {
+  //check initially if the lengths are the same
+  if (array1.length !== array2.length) {
+    return false 
+}
+  //check if all items exists and are in the same order
+for (let i = 0; i < array1.length; i++) {
+  if (array1[i] !== array2[i]) {
+    return false;
+}
+}
+return true
+};
+const assetArrayEqual= function(array1,array2) {
+  if (eqArrays(array1, array2) === true){
+  return "Pass✅ " ; 
+  }else {
+    return "Fail ❌ ";
+  }
+
+}
