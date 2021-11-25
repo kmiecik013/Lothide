3a8ccae70ee7f7039979188abd846ddb682c40a4
kmiecik013 <kmiecik013@gmail.com>
2021-11-25 01:53:32 +0000

    i think i get it

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed'  ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+
+const countLetters = function(str) {
+  const finalCount = {};
+  for (const letters of str) {
+    if (letters !== " ");
+    if (finalCount[letters]){
+      finalCount[letters] += 1
+    } else { 
+      finalCount[letters] = 1
+    }
+  } 
+
+  return finalCount
+}
+console.log(countLetters("bubble"))
