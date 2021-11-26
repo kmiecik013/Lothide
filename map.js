3e2aeb66c6865986f88eaec7eeaeaa9a271d59ae
kmiecik013 <kmiecik013@gmail.com>
2021-11-26 03:52:00 +0000

    notha one

---
+++ map.js
@@ -0,0 +1,37 @@
+
+const eqArrays = function(array1, array2) {
+  //check initially if the lengths are the same
+  if (array1.length !== array2.length) {
+    return false 
+}
+  //check if all items exists and are in the same order
+for (let i = 0; i < array1.length; i++) {
+  if (array1[i] !== array2[i]) {
+    return false;
+}
+}
+return true
+}
+const assertArraysEqual = function(array1, array2) {
+  //use eqArrays function for the array comparisson
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+const map = function(array, callback) {
+
+  const newArray = [];
+for (let item of array) {
+ newArray.push(callback(item));
+} 
+ return newArray;
+}
+const words = ["ground", "control", "to", "major", "tom"];
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+assertArraysEqual(results1, [ 'g', 'c', 't', 'm', 't' ]);
+
