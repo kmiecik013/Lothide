093931aa9d550a8c637f9a0349c32b85acaa97a4
kmiecik013 <kmiecik013@gmail.com>
2021-11-30 03:06:37 +0000

    add some more tests

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,7 +1,18 @@
+//const tail = require('../tail');
+
+const assert = require('chai').assert;
 const tail = require('../tail');
 
+describe("#tail", () => {
+  it("returns 2,3 for [1,2,3]", () => {
+    assert.deepEqual(tail([1,2,3]),[2, 3]);
+  });
+})
+it("returns '5' for ['5']", () => {
+  assert.deepEqual(tail(['5']), []); 
+});
+it("returns 2,5,6 for [1,2,4,6]", () => {
+  assert.deepEqual(tail([1,2,4,6]), [2,4,6]);
+});
 
 
-
-
-module.exports = tail
